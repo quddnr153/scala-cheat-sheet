d450564002f4fa5894f268094693a4f1e8c8b0e7
Byungwook Lee <quddnr153@gmail.com>
2018-03-11 10:50:10 +0900

    First Commit - Hello Scala

---
+++ src/main/scala/me/scala/example/HelloWorld.scala
@@ -0,0 +1,12 @@
+package me.scala.example
+
+/**
+  * @author Byungwook Lee on 2018-03-11
+  *         quddnr153@gmail.com
+  *         https://github.com/quddnr153
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, Scala world!")
+  }
+}
